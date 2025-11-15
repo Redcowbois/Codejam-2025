2837221eb6b8740cdc14d304411e7925abe53672
Jiaqi Yang <jiayang@outlook.fr>
2025-11-15 11:38:23 -0500

    added sending message mqtt

--- server/app.py
+++ server/app.py
@@ -5,13 +5,20 @@ from mqtt_handler import MQTTWorker
 from opencv import main as opencv_main
 import time
 from flask_socketio import SocketIO, emit
+import paho.mqtt.client as mqtt
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 # Global vars
 counter = 0
-event_queue = Queue()
+BROKER_HOST = "localhost"     
+BROKER_PORT = 1883
+MQTT_TOPIC_ENCOURAGEMENT = "encouragement"
+
+mqtt_client = mqtt.Client()
+mqtt_client.connect(BROKER_HOST, BROKER_PORT)
+mqtt_client.loop_start() 
 
 def start_background_threads():
     # Create threads
@@ -43,6 +50,23 @@ def mqtt_message():
     socketio.emit("counter", counter)
     return {"status": "ok"}
 
+# publish mqtt message 
+@app.route("/publish", methods=["POST"])
+def publish_message():
+    msg = "encouragement"
+    mqtt_client.publish(MQTT_TOPIC_ENCOURAGEMENT, msg)
+
+    return jsonify({
+        "status": "published",
+        "topic": MQTT_TOPIC_ENCOURAGEMENT,
+        "message": msg
+    })
+
+
+@app.route("/opencv", methods=["POST"])
+def open_cv():
+    data = request.json
+    
 
 if __name__ == "__main__":
     start_background_threads()
